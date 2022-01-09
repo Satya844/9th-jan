c4297d200654a55d3258e294fa678800219c20a7
Satyanarayan Bandha <satyanarayanbandha3@gmail.com>
2022-01-09 15:11:29 +0530

    ttt

--- array.java
+++ array.java
@@ -4,7 +4,7 @@ public class Concat {
 
     public static void main(String[] args) {
         int[] array1 = {1, 2, 4};
-        int[] array2 = {4, 5, 6};
+        int[] array2 = {4, 5, 7};
 
         int length = array1.length + array2.length;
 
