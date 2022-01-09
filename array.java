12c64258653f22252d04cc4e92027e35da9c8bd8
Satya844 <68690435+Satya844@users.noreply.github.com>
2022-01-09 15:06:05 +0530

    Update array.java

--- array.java
+++ array.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Concat {
 
     public static void main(String[] args) {
-        int[] array1 = {1, 2, 3};
+        int[] array1 = {1, 2, 4};
         int[] array2 = {4, 5, 6};
 
         int length = array1.length + array2.length;
